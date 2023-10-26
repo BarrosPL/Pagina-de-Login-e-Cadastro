96f5a6924baeb6953d4b0920155844a8c4a83328
Lucas Barros <barros.pedro@academico.ifpb.edu.br>
2023-10-26 12:55:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('container');
+const btnCadastro = document.getElementById('register');
+const btnLogin = document.getElementById('login');
+
+btnCadastro.addEventListener('click',()=>{
+    container.classList.add("active");
+});
+
+btnLogin.addEventListener('click',()=>{
+    container.classList.remove("active");
+});
+
